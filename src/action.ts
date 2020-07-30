e7fd3b7d53f564a5a821823494cffcdddf9c820f
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-07-31 01:24:35 +0300

    Change action

--- src/action.ts
+++ src/action.ts
@@ -1,3 +1,15 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
 import * as utility from './utility'
+
+export async function updateRelease(owner: string, repo: string, idOrTag: string, name: string, body: string): Promise<void> {
+  const release = await utility.getRelease(owner, repo, idOrTag)
+
+  if (name !== '') {
+    release.name = name
+  }
+
+  if (body !== '') {
+    release.body = body
+  }
+
+  await utility.updateRelease(owner, repo, release)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
 import * as utility from './utility'
+import * as action from './action'
 
 run()
 
 async function run(): Promise<void> {
   try {
-    console.log('This is a draft action.')
+    const repository = utility.getRepository()
+    const idOrTag = core.getInput('id', {required: true})
+    const name = core.getInput('name')
+    const body = core.getInput('body')
+
+    await action.updateRelease(repository.owner, repository.repo, idOrTag, name, body)
   } catch (error) {
     core.setFailed(error.message)
   }
