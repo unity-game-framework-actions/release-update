02e9e7d6ef1110de9b0d4b53cc03b48813bd4d05
Vorobey <vorobyovvorobyov@gmail.com>
2020-08-04 00:15:43 +0300

    Develop (#1)
    * Change action
    
    * Add update params
    
    * Update utility

--- src/action.ts
+++ src/action.ts
@@ -1,3 +1,22 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
 import * as utility from './utility'
+
+export async function updateRelease(owner: string, repo: string, idOrTag: string, change: any): Promise<void> {
+  const release = await utility.getRelease(owner, repo, idOrTag)
+  const updated = update(release, change)
+
+  await utility.updateRelease(owner, repo, updated)
+}
+
+function update(release: any, change: any): any {
+  const keys = Object.keys(change)
+
+  for (const key of keys) {
+    const value = change[key]
+
+    if (value !== '' && value !== false) {
+      release[key] = value
+    }
+  }
+
+  return release
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
 import * as utility from './utility'
+import * as action from './action'
 
 run()
 
 async function run(): Promise<void> {
   try {
-    console.log('This is a draft action.')
+    const repository = utility.getRepository()
+    const idOrTag = core.getInput('id', {required: true})
+    const name = core.getInput('name')
+    const body = core.getInput('body')
+    const commitish = core.getInput('commitish')
+    const draft = core.getInput('draft') === 'true'
+    const prerelease = core.getInput('prerelease') === 'true'
+
+    const change = {
+      name: name,
+      body: body,
+      commitish: commitish,
+      draft: draft,
+      prerelease: prerelease
+    }
+
+    await action.updateRelease(repository.owner, repository.repo, idOrTag, change)
   } catch (error) {
     core.setFailed(error.message)
   }
